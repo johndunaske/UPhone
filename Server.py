4a51ba3a86d3f4f582608eb2d89c85103b267ba8
johndunaske <johnduna@buffalo.edu>
2019-11-02 18:23:49 -0400

    Revert back to og
    fck me

--- Server.py
+++ Server.py
@@ -1,23 +1,15 @@
-# import socket
-#
-# HOST = 'http://35.226.102.237/'  # Standard loopback interface address (localhost)
-# PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen(10)
-# while True:
-#     conn, addr = s.accept()
-#     print('Connected by', addr)
-#     data = conn.recv(1024)
-#     if not data:
-#         break
-#     conn.sendall(data)
-# s.close()
-import bottle
+import socket
 
-@bottle.route("/")
-def home():
-    return "hewwo"
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
 
-bottle.run(host="0.0.0.0", port=8080, debug=True)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        print('Connected by', addr)
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
