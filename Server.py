b1f48130b24720ad661e8ea06b03916373631f0f
johndunaske <johnduna@buffalo.edu>
2019-11-02 17:47:05 -0400

    Logan u okay
    ddsdsdsd

--- Server.py
+++ Server.py
@@ -1,16 +1,21 @@
-import socket
+# import socket
+#
+# HOST = 'http://35.226.102.237/'  # Standard loopback interface address (localhost)
+# PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((HOST, PORT))
+# s.listen(10)
+# while True:
+#     conn, addr = s.accept()
+#     print('Connected by', addr)
+#     data = conn.recv(1024)
+#     if not data:
+#         break
+#     conn.sendall(data)
+# s.close()
+import bottle
 
-HOST = 'http://35.226.102.237/'  # Standard loopback interface address (localhost)
-PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(10)
-while True:
-    conn, addr = s.accept()
-    print('Connected by', addr)
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
-s.close()
+@bottle.route("/")
+def home():
+    return "hewwo"
